c1aface8977e5882683f930e6fa9c4ba179befde
PiroXPiyush <124165177+PiroXPiyush@users.noreply.github.com>
2023-02-10 19:55:46 +0530

    Update misc.py

--- Exon/modules/misc.py
+++ Exon/modules/misc.py
@@ -329,8 +329,8 @@ def stats(update, context):
             status
             + "\n*ʙᴏᴛ sᴛᴀᴛɪsᴛɪᴄs*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
-            + "\n\n[ɢɪᴛʜᴜʙ](https://github.com/KingAbishnoi/ExonRobot) | [ᴛᴇʟᴇɢʀᴀᴍ](https://t.me/AbishnoiMF)\n\n"
-            + f"「 ʙʏ[ᴀʙɪsʜɴᴏɪ](t.me/{AKBOSS}) 」\n",
+            + "\n\n[ɢɪᴛʜᴜʙ](https://github.com/PiroXPiyush) | [ᴛᴇʟᴇɢʀᴀᴍ](https://t.me/SexyPiyush)\n\n"
+            + f"「 ʙʏ[⏤͟͞•ᴘɪʏᴜsʜᯓ🥀](t.me/{SexyPiyush}) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
